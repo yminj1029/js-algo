1af3f4c88fa112bbb1d638269aaf768979fa5b51
MinjiYun <minjiyun204@gmail.com>
2023-05-31 10:24:06 +0900

    implement a queue using the shift() function

--- data-structure/queue-shift.js
+++ data-structure/queue-shift.js
@@ -33,41 +33,6 @@ class Queue {
     this.items = [];
   }
 }
-class Queue {
-  constructor() {
-    this.items = [];
-  }
-
-  enqueue(element) {
-    this.items.push(element);
-  }
-
-  dequeue() {
-    if (this.isEmpty()) {
-      return "Queue is empty";
-    }
-    return this.items.shift();
-  }
-
-  front() {
-    if (this.isEmpty()) {
-      return "Queue is empty";
-    }
-    return this.items[0];
-  }
-
-  isEmpty() {
-    return this.items.length === 0;
-  }
-
-  size() {
-    return this.items.length;
-  }
-
-  clear() {
-    this.items = [];
-  }
-}
 
 const queue = new Queue();
 console.log(queue.isEmpty()); // 출력: true
