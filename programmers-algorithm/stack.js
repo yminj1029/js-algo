73c6a551b4128c28e4bd089fe6caf55d5f85fabb
MinjiYun <minjiyun204@gmail.com>
2023-05-31 10:25:59 +0900

    solve parenthesis question in programmers

---
+++ programmers-algorithm/stack.js
@@ -0,0 +1,30 @@
+function solution(s) {
+  const stack = [];
+
+  for (const c of s) {
+    if (c === "(") {
+      stack.push(c);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+      stack.pop();
+    }
+  }
+  return stack.length === 0;
+}
+
+function solution2(s) {
+  let count = 0;
+  for (const c of s) {
+    if (c === "(") {
+      count += 1;
+    } else {
+      if (count === 0) {
+        return false;
+      }
+      count -= 1;
+    }
+  }
+  return count === 0;
+}
