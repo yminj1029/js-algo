72c84bf640bdccc530c17862539ccd3b334311ca
MinjiYun <minjiyun204@gmail.com>
2023-06-01 16:40:32 +0900

    add memo about linked list

--- data-structure/linked-list.js
+++ data-structure/linked-list.js
@@ -1,3 +1,25 @@
+// 연결 리스트 특징
+
+// - 메모리가 허용하는 한 요소를 제한 없이 추가할 수 있다.
+// - 탐색은 O(n)이 소요된다.
+// - 요소를 추가하거나 제거할 때는 O(1)이 소요된다.
+// - Singly Linked List, Doubly Linked List, Circular Linked List가 존재한다.
+
+// Singly Linked List
+
+// - Head에서 Tail까지 단방향으로 이어지는 연결 리스트
+// - 가장 단순한 형태인 연결 리스트다.
+
+// Doubly Linked List
+
+// 양방향으로 이어지는 연결 리스트
+
+// Singly Linked List 보다 자료 구조의 크기가 조금 더 크다
+
+// Circular Linked List
+
+// Singly 혹은 Doubly Linked List에서 Tail이 Head로 연결되는 연결리스트 메모리를 아껴쓸 수 있다. 원형 큐 등을 만들 때도 사용된다
+
 class Node {
   constructor(value) {
     this.value = value;
